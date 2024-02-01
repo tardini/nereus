baa3fd4a0d5e32dd7f8aa679462105f47a1d7ebc
Giovanni Tardini <giovanni.tardini@ipp.mpg.de>
2024-02-01 15:15:45 +0100

    Separated calc_los

--- neut_reac.py
+++ neut_reac.py
@@ -23,6 +23,7 @@ from reactivities import *
 import calc_cross_section as cs
 import calc_kinematics as ck
 import calc_spectrum as spc
+import calc_los
 import plot_rekin
 from reactions import reaction
 
@@ -36,57 +37,6 @@ logger.addHandler(hnd)
 logger.setLevel(logging.DEBUG)
 
 rekin_dir = os.path.dirname(os.path.realpath(__file__))
-
-
-class CELL:
-    pass
-
-
-class DETECTOR:
-    pass
-
-
-class PHILOS:
-
-    def __init__(self, par_d, npoints=5000):
-
-        theta  = par_d['theta']
-        phi_in = par_d['phi'] 
-        x0     = par_d['x0']
-        y0     = par_d['y0']
-        z0     = par_d['z0']
-        xend   = par_d['xend']
-        phi0 = np.arctan2(x0, y0)
-        phi  = phi_in + phi0
-        dx   = xend - x0
-        dr   = dx/np.sin(phi)
-        self.dr = np.abs(dr)
-        yend = y0 + dr*np.cos(phi)
-        zend = z0 + self.dr*np.tan(theta)
-        self.xline = np.linspace(x0, xend, npoints)
-        self.yline = np.linspace(y0, yend, npoints)
-        self.zline = np.linspace(z0, zend, npoints)
-        self.rline = np.hypot(self.xline, self.yline)
-
-
-class XYLOS:
-
-    def __init__(self, par_d, npoints=5000):
-
-        theta  = par_d['theta']
-        x0 = par_d['x0']
-        y0 = par_d['y0']
-        z0 = par_d['z0']
-        xend = par_d['xend']
-        yend = par_d['yend']
-        dx = xend - x0
-        dy = yend - y0
-        self.dr = np.hypot(dx, dy)
-        zend = z0 + self.dr*np.tan(theta)
-        self.xline = np.linspace(x0, xend, npoints)
-        self.yline = np.linspace(y0, yend, npoints)
-        self.zline = np.linspace(z0, zend, npoints)
-        self.rline = np.hypot(self.xline, self.yline)
 
 
 class REAC_GUI(QMainWindow):
@@ -484,143 +434,18 @@ class REAC_GUI(QMainWindow):
         logger.info('Starting LOS cone calculation')
         geo = self.get_gui_tab('detector')
 
-        los_d = {}
-        los_d['x0']    = -geo['y_det']
-        los_d['y0']    = 0.001
-        los_d['z0']    = geo['z_det']
-        los_d['xend']  = 0 # 1 plasma crossing
-#        los_d['xend']  = geo['y_det'] # 2 plasma crossings
-        los_d['theta'] = np.radians(geo['tilt'])
-        los_d['phi']   = -np.arctan2(geo['tan_radius'], geo['y_det'])
+        dlos = calc_los.DETECTOR_LOS(geo)
 
-        ctilt = np.cos(los_d['theta'])
-        dy = geo['disk_thick']*ctilt
-        ndisks = int(-2*geo['y_det']/dy)
-
-        self.det = DETECTOR()
-        self.det.los = PHILOS(los_d, npoints=ndisks)
-
-# Get separatrix {R,z}
-
-        rmin = geo['Rmaj']  - geo['r_chamb'] 
-        rmax = geo['Rmaj']  + geo['r_chamb'] 
-
-# Restrict to LOS inside the plasma
-
-        dl = np.hypot(self.det.los.rline[1] - self.det.los.rline[0], self.det.los.zline[1] - self.det.los.zline[0])
-        ind = (self.det.los.rline > rmin - dl) & (self.det.los.rline < rmax + dl)
-        r_in  = self.det.los.rline[ind]
-        self.det.los.z = self.det.los.zline[ind]
-        self.det.los.y = self.det.los.xline[ind]
-        y_in  = self.det.los.yline[ind]
-
-# Write ASCII file for LOS, used in the spectrum evaluation
-
-        self.det.pos = (geo['tan_radius'], geo['y_det'], geo['z_det'])
-        det_dist = np.hypot(self.det.los.y - self.det.pos[1], self.det.los.z - self.det.pos[2])
-        ctilt = np.cos(np.radians(geo['tilt']))
-        stilt = np.sin(np.radians(geo['tilt']))
-        dy = geo['disk_thick']*ctilt
-        coll_rad = 0.5*geo['coll_diam']
-        dist_corr = geo['d_det_coll']/(1. + geo['det_radius']/coll_rad) # Shifting the cone vertex to the point where lines (det-left - coll-right, det-right - coll-left) cross
-        self.det.tan_cone_aper = coll_rad/dist_corr
-#        logger.debug(coll_rad, dist_corr, tan_cone_aper)
-        offset = geo['d_det_coll'] - dist_corr # scalar, r2
-        disk_radius = (det_dist - offset)*self.det.tan_cone_aper # array on discretised LoS
-        n_disks = len(self.det.los.y)
-
-# Each disk is divided in n_circles circular sectors,
-# n_circles depends on the disk radius (which is almost constant)
-# Every circular sector is divided in n_sectors sectors,
-# equidistant poloidally; n_sectors is proportional to the radius
-# of the circular sector
-
-        self.cell = CELL()
-        self.cell.x     = []
-        self.cell.y     = []
-        self.cell.z     = []
-        self.cell.omega = []
-        self.cell.vol   = []
-
-        for jdisk in range(n_disks):
-            n_circles = int(0.5 + disk_radius[jdisk]/geo['cell_radius'])
-            delta_radius = disk_radius[jdisk]/float(n_circles)
-
-# radius, alpha in the 'middle' of the sector
-# The central circle has only one sector (cell)
-            cvol = np.pi * delta_radius**2 * dy
-            self.cell.vol = np.append(self.cell.vol, np.repeat(cvol, n_circles**2))
-
-            radius = (0.5 + np.arange(n_circles))*delta_radius
-            radius[0] = 0.
-            omega_fac = np.pi * geo['det_radius']**2/det_dist[jdisk]**3
-            cell_detDist = np.hypot(det_dist[jdisk], radius)
-            omegaCircle = omega_fac * cell_detDist
-            for j_circle in range(n_circles):
-                n_sectors = 2*j_circle + 1
-                self.cell.omega += n_sectors*[omegaCircle[j_circle]]
-# Poloidal sectors (cells) in a circle
-                alpha = np.linspace(0, 2.*np.pi, n_sectors, endpoint=False)
-                rcos  = list(radius[j_circle]*np.cos(alpha))
-                rssin = list(-self.det.los.y[jdisk] - radius[j_circle]*np.sin(alpha)*stilt)
-                rcsin = list( self.det.los.z[jdisk] + radius[j_circle]*np.sin(alpha)*ctilt)
-# cell_pos: with respect to torus center
-                self.cell.x += rcos
-                self.cell.y += rssin
-                self.cell.z += rcsin
-        self.cell.x = np.array(self.cell.x) + self.det.pos[0]
-        self.cell.y = np.array(self.cell.y)
-        self.cell.z = np.array(self.cell.z)
-        self.cell.omega = np.array(self.cell.omega)
-        logger.info('Done LOS cone calculation')
 # Plot
         if not hasattr(self, 'wid'):
             self.wid = plot_rekin.plotWindow()
-        fig_los = plot_rekin.fig_los(self.cell)
+        fig_los = plot_rekin.fig_los(dlos.cell)
         self.wid.addPlot('Detector LoS', fig_los)
         self.wid.show()
+
 # Write output
         if geo['writeLOS']:
-            self.writeLOS()
-
-
-    def writeLOS(self):
-
-        n_cells = len(self.cell.vol)
-        n_disks = len(self.det.los.y)
-        cone_aper = np.degrees(np.arctan(self.det.tan_cone_aper))
-        geo = self.get_gui_tab('detector')
-        ctilt = np.cos(np.radians(geo['tilt']))
-        dy = geo['disk_thick']*ctilt
-
-        header = \
-           'LOS\n' + \
-           '   y1 = %9.4f m\n'  % self.det.los.y[0]  + \
-           '   y2 = %9.4f m\n'  % self.det.los.y[-1] + \
-           '   z1 = %9.4f m\n'  % self.det.los.z[0]  + \
-           '   z2 = %9.4f m\n'  % self.det.los.z[-1] + \
-           'Detector:\n' + \
-           '   Position [m] x, y, z: %9.4f, %9.4f, %9.4f\n' %self.det.pos + \
-           '   Radius = %9.4f m\n' % geo['det_radius'] + \
-           '   Collimation angle = %9.4f deg\n' % cone_aper + \
-           'Disks:\n' + \
-           '   Thickness = %9.4f m\n' % dy + \
-           '   # disks = %5d\n' % n_disks + \
-           'Cells:\n' + \
-           '   Radius = %9.4f m\n' % geo['cell_radius'] + \
-           '   # cells = %d\n' %n_cells + \
-"""
-   (x,y,x) cell cartensian coordinates [m]
-   Omega  Steradians is the volume in the solid angle
-   Vol = cell volume [m**3]
-x             y             z             Omega         Vol
-"""
-
-        los_file = '%s.los' %geo['label']
-
-        logger.info('Storing ASCII output, n_cells=%d', n_cells)
-        np.savetxt(los_file, np.hstack((self.cell.x, self.cell.y, self.cell.z, self.cell.omega, self.cell.vol)).reshape(5, n_cells).T, header=header, fmt='%13.6E')
-        logger.info('Written output file %s' %los_file)
+            dlos.writeLOS()
 
 
 if __name__ == '__main__':
